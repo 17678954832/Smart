26558005982d100a107b03c53e5db0f36fa55d6e
muhan <1905240752@qq.com>
2021-12-24 10:13:16 +0800

    1.新增mybatis-generator配置

---
+++ src/main/java/com/muhan/smart/dao/OrderItemMapper.java
@@ -0,0 +1,17 @@
+package com.muhan.smart.dao;
+
+import com.muhan.smart.pojo.OrderItem;
+
+public interface OrderItemMapper {
+    int deleteByPrimaryKey(Integer id);
+
+    int insert(OrderItem record);
+
+    int insertSelective(OrderItem record);
+
+    OrderItem selectByPrimaryKey(Integer id);
+
+    int updateByPrimaryKeySelective(OrderItem record);
+
+    int updateByPrimaryKey(OrderItem record);
+}
